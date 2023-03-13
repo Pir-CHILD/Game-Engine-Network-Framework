751ad5a663eba78c5104992c35d98ba2af38ba77
zhao qing <pir.child@gmail.com>
2023-03-13 17:15:39 +0800

    feat: add init communication file.

---
+++ client_udp.h
@@ -0,0 +1,16 @@
+#ifndef _CLIENT_UDP
+#define _CLIENT_UDP
+
+#include <cstdio>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <cstring>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define SERVER_PORT 12306
+#define BUFF_LEN 1024
+#define SERVER_IP "127.0.0.1"
+
+#endif
---
+++ server_udp.h
@@ -0,0 +1,14 @@
+#ifndef _SERVER_UDP
+#define _SERVER_UDP
+
+#include <cstdio>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <cstring>
+#include <unistd.h>
+
+#define SERVER_PORT 12306
+#define BUFF_LEN 1024
+
+#endif
